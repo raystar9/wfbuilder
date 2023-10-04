e7b9777272de8f11f6d88b28aff97bc0e6489e10
Raystar9 <raystarkoo@gmail.com>
2023-10-04 23:35:44 +0900

    11

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:3002", "http://www.wf-builder.kro.kr"]
+    origin: ["http://localhost:3002", "http://www.wf-builder.kro.kr:3002", "http://wf-builder.kro.kr:3002"]
   });
   await app.listen(3000);
 }
